4e9a2cdd0e52b5906b944a3e08f335133640b687
Ezra1285 <ezraskoog@yahoo.com>
2025-10-18 20:53:48 -0600

    Added start of navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,19 @@
 import {Component} from '@angular/core';
 import {Home} from './home/home';
+import { Navbar } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Home],
+  imports: [Home, Navbar],
   template: `
+  
+  
     <main>
+    
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
       </header>
+      <app-navbar></app-navbar>   
       <section class="content">
         <app-home></app-home>
       </section>
