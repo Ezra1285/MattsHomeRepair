7bd56fb586645b019d138db3804e53416bc1b826
Ezra1285 <ezraskoog@yahoo.com>
2025-10-21 19:54:11 -0600

    Added service content

--- src/app/services/services.ts
+++ src/app/services/services.ts
@@ -4,7 +4,123 @@ import { Component } from '@angular/core';
   selector: 'app-services',
   imports: [],
   template: `
-    <h1>Services Works</h1>
+    
+
+<section class="jumbotron text-center">
+<div class="container">
+  <h1 class="jumbotron-heading">All Services Offered</h1>
+  <p class="lead text-muted">Shelving and Hooks
+Hanging Pictures and TVs,
+Interior⁄Exterior Door Installation,
+Doggie Door Installation,
+Door Knobs and Locks,
+Door and Baseboard Trim,
+Drywall Patching,
+Appliance Installation,
+Light Fixtures and Ceiling Fans,
+Outlets and Switches,
+Toilets ⁄ Sinks ⁄ Faucets,
+Grow Room Construction,
+Garden Boxes,
+Tree Trimming or Removal,
+Small Roofing Jobs,
+Small Construction Jobs,
+Custom Projects,
+Trips to the Garbage Dump</p>
+  <p>
+	<a href="contact" class="btn btn-primary my-2">Request a Quote</a>
+  </p>
+</div>
+</section>
+
+<div class="album py-5 bg-light">
+<div class="container">
+  <div class="row">
+	<div class="col-md-4">
+	  <div class="card mb-4 box-shadow">
+		<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22509%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20509%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_19a09708d6b%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_19a09708d6b%22%3E%3Crect%20width%3D%22509%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22170.24478912353516%22%20y%3D%22123.70000076293945%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+		<div class="card-body">
+		  <p class="card-text">Toilets/Sinks/Faucets</p>
+		  <div class="d-flex justify-content-between align-items-center">
+			<div class="btn-group">
+			  <button type="button" class="btn btn-sm btn-outline-secondary">View More</button>  
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+	<div class="col-md-4">
+	  <div class="card mb-4 box-shadow">
+		<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22509%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20509%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_19a09708d6d%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_19a09708d6d%22%3E%3Crect%20width%3D%22509%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22170.24478912353516%22%20y%3D%22123.70000076293945%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+		<div class="card-body">
+		  <p class="card-text">Drywall patching</p>
+		  <div class="d-flex justify-content-between align-items-center">
+			<div class="btn-group">
+			  <button type="button" class="btn btn-sm btn-outline-secondary">View More</button>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+	<div class="col-md-4">
+	  <div class="card mb-4 box-shadow">
+		<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22509%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20509%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_19a09708d6e%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_19a09708d6e%22%3E%3Crect%20width%3D%22509%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22170.24478912353516%22%20y%3D%22123.70000076293945%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+		<div class="card-body">
+		  <p class="card-text">Tree Trimming</p>
+		  <div class="d-flex justify-content-between align-items-center">
+			<div class="btn-group">
+			  <button type="button" class="btn btn-sm btn-outline-secondary">View More</button>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+
+	<div class="col-md-4">
+	  <div class="card mb-4 box-shadow">
+		<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22509%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20509%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_19a09708d6e%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_19a09708d6e%22%3E%3Crect%20width%3D%22509%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22170.24478912353516%22%20y%3D%22123.70000076293945%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+		<div class="card-body">
+		  <p class="card-text">Doors and locks</p>
+		  <div class="d-flex justify-content-between align-items-center">
+			<div class="btn-group">
+			  <button type="button" class="btn btn-sm btn-outline-secondary">View More</button>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+	<div class="col-md-4">
+	  <div class="card mb-4 box-shadow">
+		<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22509%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20509%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_19a09708d6f%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_19a09708d6f%22%3E%3Crect%20width%3D%22509%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22170.24478912353516%22%20y%3D%22123.70000076293945%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+		<div class="card-body">
+		  <p class="card-text">Sprinkler repair</p>
+		  <div class="d-flex justify-content-between align-items-center">
+			<div class="btn-group">
+			  <button type="button" class="btn btn-sm btn-outline-secondary">View More</button>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+	<div class="col-md-4">
+	  <div class="card mb-4 box-shadow">
+		<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22509%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20509%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_19a09708d6f%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_19a09708d6f%22%3E%3Crect%20width%3D%22509%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22170.24478912353516%22%20y%3D%22123.70000076293945%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 225px; width: 100%; display: block;">
+		<div class="card-body">
+		  <p class="card-text">Shelving Home / Garage</p>
+		  <div class="d-flex justify-content-between align-items-center">
+			<div class="btn-group">
+			  <button type="button" class="btn btn-sm btn-outline-secondary">View More</button>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </div>
+</div>
+</div>
+
+
+
   `,
   styles: ``
 })
